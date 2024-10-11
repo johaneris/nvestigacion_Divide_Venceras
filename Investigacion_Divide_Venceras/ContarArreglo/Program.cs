a254ecda2abf081b9d57696e28c30c798227638f
johaneris <jsavalos@uamv.edu.ni>
2024-10-11 06:56:10 -0600

    ejercicio e informe

---
+++ Investigacion_Divide_Venceras/ContarArreglo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ContarArreglo
+{
+    internal class Program
+    {
+        static int Count(int[] arr, int index)
+        {
+            if (index == arr.Length)
+                return 0;
+
+            return 1 + Count(arr, index + 1);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int count = Count(arr, 0);
+            Console.WriteLine("El numero de elementos en el arreglo es: " + count);
+            Console.ReadKey();
+        }
+    }
+}
